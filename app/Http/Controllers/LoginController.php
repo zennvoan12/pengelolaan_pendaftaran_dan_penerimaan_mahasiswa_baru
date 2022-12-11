e41348af5c9fa9c8bde0e6a207ca4c1fa4767709
M Farhan Novian <inside.suck@gmail.com>
2022-12-11 20:45:05 +0700

    tambah pop up pada login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,37 +7,52 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+
+
+
     public function login()
     {
-        return view ('auth.login');
+        return view('auth.login');
     }
+
+
 
     public function authenticate(Request $request)
     {
+        $notification1 = [
+            'message' => 'Anda Berhasil Login',
+            'alert-type' => 'success'
+        ];
+        $notification2 = [
+            'message' => 'Mungkin Terjadi Kesalahan Coba Cek Kembali',
+            'alert-type' => 'error'
+        ];
         if (Auth::attempt(['username' => $request->username, 'password' => $request->password]) || Auth::attempt(['email' => $request->username, 'password' => $request->password])) {
+
             $request->session()->regenerate();
- 
-            return redirect()->intended('dashboard');
+
+            return redirect()->intended('dashboard')->with($notification1);
         }
- 
+
+
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
-        ]);
+        ])->with($notification2);
     }
     public function logout(Request $request)
     {
         Auth::logout();
-    
+
         $request->session()->invalidate();
-    
+
         $request->session()->regenerateToken();
-    
+
         return redirect('/login');
     }
     public function forgetpass()
     {
-        
-    
+
+
         return view('auth.forgetpass');
     }
 }
